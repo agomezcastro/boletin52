a4540f97f5f603c1078e9b4ab1451baa5a72a646
agomezcastro <agomezcastro@atanasoff05.danielcastelao.org>
2015-10-08 09:25:43 +0200

    presentacion añadido frase

--- src/proyectoalfa/ProyectoAlfa.java
+++ src/proyectoalfa/ProyectoAlfa.java
@@ -8,6 +8,7 @@ public class ProyectoAlfa {
 
     public static void main(String[] args) {
         System.out.println("Hola, me llamo proyecto: Alfa");
+        System.out.println("Trateme bien");
     }
     
 }
