42af6a749eaaf0252a7b3409c83c2389ee8f7ee0
agomezcastro <agomezcastro@atanasoff05.danielcastelao.org>
2015-10-08 09:37:22 +0200

    presentacion añadido frase espacios

--- src/proyectoalfa/ProyectoAlfa.java
+++ src/proyectoalfa/ProyectoAlfa.java
@@ -9,6 +9,8 @@ public class ProyectoAlfa {
     public static void main(String[] args) {
         System.out.println("Hola, me llamo proyecto: Alfa");
         System.out.println("Trateme bien");
+        
+        
     }
     
 }
